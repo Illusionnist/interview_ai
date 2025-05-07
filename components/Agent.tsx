d4d83eb306477c79e0279dc839e4be0d8922a94b
Illusionnist <goldchief@yahoo.com>
2025-05-08 00:49:37 +1000

    Final stage

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import {cn} from "@/lib/utils";
 import {useRouter} from "next/navigation";
 import {vapi} from "@/lib/vapi.sdk";
 import {interviewer} from "@/constants";
+import {createFeedback} from "@/lib/actions/general.action";
 
 
 enum CallStatus {
@@ -15,7 +16,7 @@ enum CallStatus {
     FINISHED = "FINISHED",
 }
 
-interface SavedMesssages{
+interface SavedMesssages {
     role: 'user' | 'assistant' | 'system';
     content: string;
 }
@@ -27,20 +28,20 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
     const [messages, setMessages] = useState<SavedMesssages[]>([])
 
     useEffect(() => {
-        const onCallStart = () =>  setCallStatus(CallStatus.ACTIVE);
-        const onCallEnd = () =>  setCallStatus(CallStatus.FINISHED);
+        const onCallStart = () => setCallStatus(CallStatus.ACTIVE);
+        const onCallEnd = () => setCallStatus(CallStatus.FINISHED);
 
         const onMessage = (message: Message) => {
-            if (message.type === 'transcript' && message.transcriptType === 'final'){
-                const newMessage = { role: message.role, content: message.transcript}
+            if (message.type === 'transcript' && message.transcriptType === 'final') {
+                const newMessage = {role: message.role, content: message.transcript}
 
-                setMessages((prev) =>  [...prev, newMessage]);
+                setMessages((prev) => [...prev, newMessage]);
             }
         }
 
         const onSpeechStart = () => setIsSpeaking(true);
-        const  onSpeechEnd = () => setIsSpeaking(false);
-        const onError  = (error:Error) => console.log('Error', error);
+        const onSpeechEnd = () => setIsSpeaking(false);
+        const onError = (error: Error) => console.log('Error', error);
 
         vapi.on('call-start', onCallStart);
         vapi.on('call-end', onCallEnd)
@@ -50,7 +51,7 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
         vapi.on('error', onError);
 
 
-        return() =>{
+        return () => {
             vapi.off('call-start', onCallStart);
             vapi.off('call-end', onCallEnd);
             vapi.off('message', onMessage);
@@ -62,22 +63,27 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
 
 
     const handleGenerateFeedback = async (messages: SavedMesssages[]) => {
-        console.log('Generate Feedback Here');
-        const {success, id} = {
-            success: true,
-            id: 'Feedback-id'
-        }
 
-        if(success && id){
+
+        console.log('Generate Feedback Here');
+
+
+        const {success, feedbackId : id} = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        })
+
+        if (success && id) {
             router.push(`/interview/${interviewId}/feedback`)
-        }else {
+        } else {
             console.log('Error generating feedback')
             router.push(`/`)
         }
     }
 
     useEffect(() => {
-        if(callStatus === CallStatus.FINISHED){
+        if (callStatus === CallStatus.FINISHED) {
             if (type === 'generate') {
                 router.push(`/`)
             } else {
@@ -86,33 +92,35 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
         }
     }, [messages, callStatus, type, userId]);
 
-    const handleCall = async  () => {
-    setCallStatus(CallStatus.CONNECTING);
-    if(type === 'generate'){
-        await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,  {
-            variableValues: {
-                username: userName,
-                userid: userId
+    const handleCall = async () => {
+        setCallStatus(CallStatus.CONNECTING);
+        if (type === 'generate') {
+            await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+                variableValues: {
+                    username: userName,
+                    userid: userId
+                }
+            })
+        } else {
+            let formattedQuestions = '';
+            if (questions) {
+                formattedQuestions = questions
+                    .map((questions) => {
+                        `- ${questions}`
+                    }).join('\n');
             }
-        } )
-    }else {
-        let formattedQuestions  = '';
-        if(questions){
-            formattedQuestions = questions
-                .map((questions) => {`- ${questions}`}).join('\n');
+            await vapi.start(interviewer, {
+                variableValues: {
+                    questions: formattedQuestions,
+                },
+            });
         }
-        await vapi.start(interviewer, {
-            variableValues: {
-                questions: formattedQuestions,
-            },
-        });
-    }
 
 
     }
 
 
-    const handleDisconnect = async  () => {
+    const handleDisconnect = async () => {
         setCallStatus(CallStatus.FINISHED);
         vapi.stop()
     }
@@ -140,7 +148,8 @@ const Agent = ({userName, userId, type, interviewId, questions}: AgentProps) => 
             {messages.length > 0 && (
                 <div className="transcript-border">
                     <div className="transcript">
-                        <p key={lastMessage} className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>{lastMessage}</p>
+                        <p key={lastMessage}
+                           className={cn('transition-opacity duration-500 opacity-0', 'animate-fadeIn opacity-100')}>{lastMessage}</p>
                     </div>
                 </div>
             )}
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,4 +1,9 @@
+'use server'
+
 import {db} from "@/firebase/admin";
+import {generateObject} from "ai";
+import {google} from "@ai-sdk/google";
+import {feedbackSchema} from "@/constants";
 
 export async function getInterviewByUserId(userID: string): Promise<Interview[] | null> {
     const interviews = await db.collection('interviews')
@@ -6,10 +11,10 @@ export async function getInterviewByUserId(userID: string): Promise<Interview[] 
         .orderBy('createdAt', 'desc')
         .get();
 
-    return interviews.docs.map((doc)=>({
+    return interviews.docs.map((doc) => ({
         id: doc.id,
         ...doc.data()
-    })) as  Interview[];
+    })) as Interview[];
 }
 
 export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
@@ -24,10 +29,10 @@ export async function getLatestInterviews(params: GetLatestInterviewsParams): Pr
             .limit(limit)
             .get();
 
-    return interviews.docs.map((doc)=>({
+    return interviews.docs.map((doc) => ({
         id: doc.id,
         ...doc.data()
-    })) as  Interview[];
+    })) as Interview[];
 }
 
 export async function getInterviewById(id: string): Promise<Interview | null> {
@@ -36,4 +41,78 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
         .get();
 
     return interview.data() as Interview | null;
+}
+
+export async function createFeedback(params: CreateFeedbackParams) {
+    const {interviewId, userId, transcript} = params;
+
+
+    try {
+
+        const formattedTranscript = transcript
+            .map((sectence: { role: string; content: string }) => (
+                `- ${sectence.role}: ${sectence.content}\n`
+            )).join('');
+
+        const {object: {totalScore, categoryScores, strengths, areasForImprovement, finalAssessment}} = await generateObject({
+            model: google('gemini-2.5-pro-exp-03-25',
+                {structuredOutputs: false,}),
+            schema: feedbackSchema,
+            prompt: `
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
+            system:
+                "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+        });
+
+        const feedback = await db.collection('feedback').add({
+            interviewId,
+            userId,
+            totalScore,
+            categoryScores,
+            strengths,
+            areasForImprovement,
+            finalAssessment,
+            createdAt: new Date().toISOString(),
+        })
+
+        return{
+            success: true,
+            feedbackId: feedback.id
+        }
+
+    } catch (e) {
+        console.log('Error creating feedback', e)
+
+        return { success: false}
+    }
+}
+
+export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
+
+    const {interviewId, userId} = params;
+
+    const feedback = await
+        db.collection('feedback')
+            .where('interviewId', '==', interviewId)
+            .where('userId', '==', userId)
+            .limit(1)
+            .get();
+    if(feedback.empty) return null;
+
+    const feedbackDoc = feedback.docs[0]
+
+   return {
+        id: feedbackDoc.id,
+        ...feedbackDoc.data()
+    } as Feedback;
 }
